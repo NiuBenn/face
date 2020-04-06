c61e09c3b160b2119e8c30e22996957736ba6eec
NiuBenn <1402435102@qq.com>
2020-04-06 20:27:38 +0800

    .

--- server/server.cc
+++ server/server.cc
@@ -1,6 +1,23 @@
 #include "httplib.h"
+#include "../data/data.hpp"
+#include  <cstring>
+
+std::map<string, face_encoding> m_face_encoding;
+
+MYSQL mysql;   
+MYSQL_ROW row;  
+MYSQL_FIELD* field = NULL;       
+MYSQL_RES* result;   
 
 int main() {
+
+    mysql_init(&mysql);
+    if(!mysql_real_connect(&mysql,"localhost","root","root","face",0,NULL,0))
+    {     
+        cout << "connect fial\n";
+        return -1;
+    }
+
     using namespace httplib;
     Server server;
 
@@ -19,6 +36,45 @@ int main() {
             res.set_content(result, "text/html");
         }
     });
+
+    server.Post("/create", [](const Request& req,
+                Response& res){
+                std::string name = req.get_param_value("name");
+                std::string money = req.get_param_value("id");
+                std::string number = req.get_param_value("number");
+                std::cout << "Get Submit!" <<std::endl;
+
+                if(face_encoding.count(number) > 0) {
+                    time_t now_time = get_time();
+                    if(now_time - face_encoding[number]._time > 600){
+                        res.set_content("注册码已失效", "text/html");
+                    }
+                    else {
+                        char* data = face_encoding[number]._data;
+                        bool ret = registered_face(&mysql, data, name, id);
+                        if (ret) {
+                            res.set_content("注册成功", "text/html");
+                        }
+                    }
+                    face_encoding.erase(number);
+                }
+                else {
+                    res.set_content("无效验证码", "text/html");
+                }
+             });
+
+    server.Post("/registered_face", [](const Request& req,
+                Response& res){
+            std::string body = req.body;
+            std::string number = get_number()
+            
+            memcmp(m_face_encoding[number]._data, body.data(), body.length());
+            m_face_encoding[number]._time = get_time();
+            
+            res.set_content(number, "text/html");
+    });
     
     server.listen("0.0.0.0", 9000);
+
+    mysql_close(&mysql);
 }
